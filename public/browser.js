a72d9ee16b767a1e098ddbd32329900059f22add
Chaka Hongoke <hongoke999@yahoo.com>
2021-02-25 05:27:27 +0200

    Updating a Database Item (Part 1)

---
+++ public/browser.js
@@ -0,0 +1,11 @@
+
+document.addEventListener("click", function(e) {
+    if(e.target.classList.contains("edit-me")) {
+        let userInput = prompt("Enter your desired new text")
+        axios.post('/update-item', {text: userInput}).then(function() {
+            //  do something interesting here in the next video
+        }).catch(function() {
+            console.log("Please try again later.")
+        })
+    }
+})
--- server.js
+++ server.js
@@ -4,12 +4,15 @@ let mongodb = require('mongodb')
 let app = express()
 let db
 
+app.use(express.static('public'))
+
 let connectionString = 'mongodb+srv://todoAppUser:todoklo12@cluster0.ijr3a.mongodb.net/TodoApp?retryWrites=true&w=majority'
 mongodb.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
     db = client.db()
     app.listen(3000)
 })
 
+app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.get('/', function(req, res) {
@@ -37,7 +40,7 @@ app.get('/', function(req, res) {
                 </div>
                 
                 <ul class="list-group pb-5">
-                    ${items.map(function(item) {
+                    ${items.map(function (item) {
                         return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
                         <span class="item-text">${item.text}</span>
                         <div>
@@ -45,11 +48,13 @@ app.get('/', function(req, res) {
                         <button class="delete-me btn btn-danger btn-sm">Delete</button>
                         </div>
                         </li>`;
-                    }).join('')}
+                      }).join('')}
                 </ul>
                 
             </div>
             
+            <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+            <script src="/browser.js"></script>
             </body>
             </html>
         `); 
@@ -61,5 +66,10 @@ app.post('/create-item', function(req, res) {
     db.collection('items').insertOne({text: req.body.item}, function() {
         res.redirect('/')
     })    
+})
+
+app.post('/update-item', function(req, res) {
+    console.log(req.body.text)
+    res.send("Success")
 })
 
