54d9ed2cf013fb531c7b0c3b09c298493b6b473d
Chaka Hongoke <hongoke999@yahoo.com>
2021-02-26 08:52:51 +0200

    Client-Side Rendering

--- public/browser.js
+++ public/browser.js
@@ -10,6 +10,12 @@ function itemTemplate(item) {
     `;
 }
 
+//  Initial Page Load Render
+let ourHTML = items.map(function(item) {
+    return itemTemplate(item)
+}).join("")
+document.getElementById("item-list").insertAdjacentHTML("beforeend", ourHTML)
+
 // Create Feature
 let createField = document.getElementById("create-field")
 
